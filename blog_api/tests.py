56ba599ee3079a36fceef75dc836fec2dcbf5b0c
default-007 <brianokola@gmail.com>
2022-11-09 21:34:37 +0300

    Add post update for author

--- blog_api/tests.py
+++ blog_api/tests.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from blog.models import Post, Category
 from django.contrib.auth.models import User
+from rest_framework.test import APIClient
 
 
 class PostTests(APITestCase):
@@ -31,4 +32,38 @@ class PostTests(APITestCase):
 
         root = reverse(("blog_api:detailcreate"), kwargs={"pk": 1})
         response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_update(self):
+        client = APIClient()
+
+        self.test_category = Category.objects.create(name="django")
+        self.testuser1 = User.objects.create_user(
+            username="testuser1", password="1234567"
+        )
+        self.testuser2 = User.objects.create_user(
+            username="testuser2", password="1234567"
+        )
+        test_post = Post.objects.create(
+            category_id=1,
+            title="Post title",
+            excerpt="Post description",
+            content="Post description",
+            author_id=1,
+        )
+        client.login(username=self.testuser1.username, password="1234567")
+
+        url = reverse(("blog_api:detailcreate"), kwargs={"pk": 1})
+
+        response = client.put(
+            url,
+            {
+                "title": "Post title",
+                "author": 1,
+                "excerpt": "Post description",
+                "content": "Post description",
+            },
+            format="json",
+        )
+        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
