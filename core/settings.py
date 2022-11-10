16caa7252b68f42c41ee38bbacee0e3f9c53bebc
default-007 <brianokola@gmail.com>
2022-11-10 17:14:04 +0300

    Add simple JWT for token authentication

--- core/settings.py
+++ core/settings.py
@@ -90,7 +90,10 @@ DATABASES = {
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
-    ]
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
+    ],
 }
 
 # Password validation
