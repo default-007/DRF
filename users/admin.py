7b64133ee9e84ba256e7dc60cdfee4cae3b2cc5f
default-007 <brianokola@gmail.com>
2022-11-10 18:35:08 +0300

    Register new model in admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from users.models import NewUser
+from django.contrib.auth.admin import UserAdmin
+from django.forms import TextInput, Textarea, CharField
+from django import forms
+from django.db import models
 
-# Register your models here.
+
+class UserAdminConfig(UserAdmin):
+    model = NewUser
+    search_fields = ("email", "first_name", "user_name")
+    list_filter = ("email", "first_name", "user_name", "is_staff", "is_active")
+    ordering = ("-start_date",)
+    list_display = ("email", "first_name", "user_name", "is_staff", "is_active")
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "first_name",
+                    "user_name",
+                ),
+            },
+        ),
+        ("Permissions", {"fields": ("is_staff", "is_active")}),
+        ("Personal", {"fields": ("about",)}),
+    )
+    formfield_overrides = {
+        models.TextField: {"widget": Textarea(attrs={"rows": 20, "cols": 60})}
+    }
+    add_fieldsets = (
+        None,
+        {
+            "classes": ("wide",),
+            "fields": (
+                "email",
+                "user_name",
+                "first_name",
+                "password1",
+                "password2",
+                "is_active",
+            ),
+        },
+    )
+
+
+admin.site.register(NewUser, UserAdminConfig)
