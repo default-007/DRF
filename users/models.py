859af5111f24efd8d590eda281e9762921fa07eb
default-007 <brianokola@gmail.com>
2022-11-10 17:38:33 +0300

    Add new user class

--- users/models.py
+++ users/models.py
@@ -35,3 +35,21 @@ class CustomAccountManager(BaseUserManager):
         user.set_password(password)
         user.save()
         return user
+
+
+class NewUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("Email Address"), unique=True)
+    user_name = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150, unique=True)
+    start_date = models.DateTimeField(default=timezone.now)
+    about = models.TextChoices(_("about"), max_length=500, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    objects = CustomAccountManager()
+
+    USERNAME_FIELDS = "email"
+    REQUIRED_FIELDS = [
+        "user_name",
+        "first_name",
+    ]
